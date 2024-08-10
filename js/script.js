5f72b5178e3b6258b1160b3d492805a9e2188f5b
MAS0024 <sandoval.mas23@gmail.com>
2024-08-10 12:53:12 -0300

    STVLibre
    STVLibre v0.1

---
+++ js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const listVideo = document.querySelectorAll('.video-list .vid');
+    const mainVideo = document.querySelector('.main-video iframe');
+    const title = document.querySelector('.main-video .title');
+    const searchBar = document.getElementById('search');
+
+    listVideo.forEach(video => {
+        video.addEventListener('click', () => {
+            listVideo.forEach(vid => vid.classList.remove('active'));
+            video.classList.add('active');
+            if (video.classList.contains('active')) {
+                const src = video.dataset.src;
+                mainVideo.src = src;
+                title.textContent = video.querySelector('.title').textContent;
+            }
+        });
+    });
+
+    searchBar.addEventListener('input', () => {
+        const filter = searchBar.value.toLowerCase();
+        listVideo.forEach(video => {
+            const title = video.getAttribute('data-title').toLowerCase();
+            video.style.display = title.includes(filter) ? '' : 'none';
+        });
+    });
+});
